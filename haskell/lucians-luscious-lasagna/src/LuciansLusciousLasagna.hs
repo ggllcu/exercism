54876d6f27d236bfbddf96000047ab026fe01317
Luca Guglielmi <luca.guglielmi@intesys.it [push]>
2022-02-20 03:51:38 +0100

    space-age

--- haskell/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
+++ haskell/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
@@ -1,7 +1,13 @@
 module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
 
 -- TODO: define the expectedMinutesInOven constant
+expectedMinutesInOven :: Int
+expectedMinutesInOven = 40
 
 -- TODO: define the preparationTimeInMinutes function
+preparationTimeInMinutes :: Int -> Int
+preparationTimeInMinutes layer = layer * 2
 
 -- TODO: define the elapsedTimeInMinutes function
+elapsedTimeInMinutes :: Int -> Int -> Int
+elapsedTimeInMinutes layer cookedTime = preparationTimeInMinutes layer + expectedMinutesInOven - cookedTime
